9d79aa82724792003b4eab672a03e7a67772ce2d
Erik Seguinte <ErikSeguinte@gmail.com>
2020-02-10 18:58:18 -0800

    main and test fi;es

---
+++ main.py
@@ -0,0 +1,9 @@
+class Main():
+    def __init__(self):
+        self.cave
+        self.player
+
+
+
+if __name__ == "__main__":
+    main = Main()
---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+import main.py
+
+class TestMain(unittest.TestCase):
+
+    # TODO
+    print(testing)
+    self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
